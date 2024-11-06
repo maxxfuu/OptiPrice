50853a2d9ba76b3eaa548d9420e3a3ca53090cef
Max Fu <mfu27@ucmerced.edu>
2024-11-06 15:56:41 -0800

    Setting up PostgrSQL and option pricer

---
+++ build/CMakeFiles/OptiPrice.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OptiPrice.dir/src/main.cpp.o"
+  "CMakeFiles/OptiPrice.dir/src/main.cpp.o.d"
+  "CMakeFiles/OptiPrice.dir/src/option_pricer.cpp.o"
+  "CMakeFiles/OptiPrice.dir/src/option_pricer.cpp.o.d"
+  "CMakeFiles/OptiPrice.dir/src/user_auth.cpp.o"
+  "CMakeFiles/OptiPrice.dir/src/user_auth.cpp.o.d"
+  "OptiPrice"
+  "OptiPrice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OptiPrice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OptiPrice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OptiPrice.
+# This may be replaced when dependencies are built.
